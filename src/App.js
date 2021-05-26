ef2a67b394c547a7cb65afb564f6d1c766e3ecc6
william-coulter <z5113817@ad.unsw.edu.au>
2021-05-26 13:50:48 +1000

    Change NavBar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
             <Route path="/about">
               <About />
             </Route>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,33 @@
 import { Link, useLocation } from "react-router-dom";
 import Up from "../icons/chevron-up.svg";
 
+const paths = ["Home", "Resume", "Articles", "About"];
+const DEFAULT_PATH = paths[0];
+
 function Navbar() {
   const location = useLocation();
-  const paths = ["Home", "Resume", "Articles", "About"];
+  const currentPath =
+    location.pathname.slice(1) === ""
+      ? DEFAULT_PATH
+      : location.pathname.slice(1);
 
   return (
     <div>
       <div class="bg-green-500 h-32 -mt-8 -mx-4 flex flex-row justify-center content-center shadow-md">
         {paths.map((p) => (
-          <StyledLink name={p} location={location} />
+          <StyledLink name={p} current={currentPath} />
         ))}
       </div>
-      <div class="w-6/12 mx-auto -mt-10">
-        <HeaderCard heading={location.pathname.slice(1)} />
+      <div class="w-52 mx-auto -mt-10">
+        <HeaderCard heading={currentPath} />
       </div>
     </div>
   );
 }
 
-function StyledLink({ name, location }) {
-  const selected = name === location.pathname.slice(1);
+function StyledLink({ name, current }) {
+  const selected = name === current;
+
   return (
     <div class="flex flex-col justify-center content-center px-4">
       <Link
